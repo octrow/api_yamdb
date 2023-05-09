d29b4853f1dd1d4443ba3b7ca34c6a61667c9363
Daniil Petrov <octrow@yandex.ru>
2023-05-09 17:54:13 +0600

    update

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def year_validator(value):
     current_year = datetime.datetime.now().year
-    if value < -33000 or value > current_year:
+    if value < 0 or value > current_year:
         raise ValidationError(
             f'{value} не подходит для года, попробуйте еще раз'
         )
