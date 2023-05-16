15a634475b72ead16d2040f34610d846e9fbf420
Daniil Petrov <octrow@yandex.ru>
2023-05-16 17:56:00 +0600

    update

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -7,6 +7,7 @@ class TitleFilter(FilterSet):
     category = CharFilter(field_name="category__slug", lookup_expr="iexact")
     genre = CharFilter(field_name="genre__slug", lookup_expr="iexact")
     name = CharFilter(field_name="name", lookup_expr="icontains")
+    year = CharFilter(field_name="year", lookup_expr="iexact")
 
     class Meta:
         model = Title
