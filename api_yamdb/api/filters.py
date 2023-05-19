03baf274d0ca913bfdbd500d4a6c1456e8ea5d28
Daniil Petrov <octrow@yandex.ru>
2023-05-20 01:30:38 +0600

    minor fix

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,13 +1,19 @@
-from django_filters import CharFilter, FilterSet
+from django_filters import CharFilter, FilterSet, ChoiceFilter
 
 from reviews.models import Title
+from api_yamdb import constances
 
 
 class TitleFilter(FilterSet):
     category = CharFilter(field_name="category__slug", lookup_expr="iexact")
     genre = CharFilter(field_name="genre__slug", lookup_expr="iexact")
     name = CharFilter(field_name="name", lookup_expr="icontains")
-    year = CharFilter(field_name="year", lookup_expr="iexact")
+    year = ChoiceFilter(
+        field_name="year",
+        choices=[
+            (y, y) for y in range(constances.MINYEAR, constances.CURRENTYEAR)
+        ],
+    )
 
     class Meta:
         model = Title
