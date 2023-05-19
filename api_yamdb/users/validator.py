8fd4a9a43448a0279505be381cd6980dffacd80e
Daniil Petrov <octrow@yandex.ru>
2023-05-19 18:20:08 +0600

    fix another mistake

--- api_yamdb/users/validator.py
+++ api_yamdb/users/validator.py
@@ -1,6 +1,7 @@
 import re
 
 from rest_framework.serializers import ValidationError
+from api_yamdb import constances
 
 
 def username_valid(value):
@@ -11,11 +12,16 @@ def username_valid(value):
     prohibited_usernames = ("me",)
     if value in prohibited_usernames:
         raise ValidationError("Недопустимое имя пользователя")
-    pattern = r"^[\w.@+-]+\Z"  # Это константа, ей место в файле для констант.
-    if not re.search(pattern, value):
-        raise ValidationError("Только буквы, цифры и символы @/./+/-/_ .")  #
-        # Какой конкретно символ я ввел не тот? Я - простой пользователь не
-        # знаю, а разбираться в этих слешах минусах и плюсах, я не хочу,
-        # загуглю другой сервис. Используем re.sub, чтобы найти всю запрещенку
-        # и вывести её в исключении.
+    # pattern = r"^[\w.@+-]+\Z"
+    # ГОТОВО! Это константа, ей место в файле для констант.
+    if not re.search(constances.PATTERN, value):
+        forbidden_chars = re.sub(constances.PATTERN, "", value)
+        raise ValidationError(
+            f"Только буквы, цифры и символы @/./+/-/_ ."
+            f"Вы ввели: {forbidden_chars}"
+        )
+        # ГОТОВО! Какой конкретно символ я ввел не тот? Я - простой
+        # пользователь не знаю, а разбираться в этих слешах минусах и плюсах,
+        # я не хочу, загуглю другой сервис. Используем re.sub, чтобы найти всю
+        # запрещенку и вывести её в исключении.
     return value
